4837978edcc34529a58991bb2ea0b69b731c155f
no-a-name <3866462668@qq.com>
2025-10-08 14:54:16 +0800

    Add files via upload

--- picture_to_text.py
+++ picture_to_text.py
@@ -19,17 +19,19 @@ st.write("上传一张图片，自动生成ASCII画！")
 
 uploaded_file = st.file_uploader("选择一张图片，后缀名要为.jpg", type=["jpg"])
 
+cols = st.number_input("输出宽度（字符数）", min_value=10, max_value=300, value=150, step=2)
+
 if uploaded_file is not None:
     ascii_chars = list("@#S%?*+;:,. ")
     img = Image.open(uploaded_file)
-    w_percent = 150 / float(img.size[0])
+    w_percent = cols / float(img.size[0])
     height = int(float(img.size[1]) * float(w_percent * 0.5))
-    img = img.resize((150, height), Image.NEAREST)
+    img = img.resize((cols, height), Image.NEAREST)
     img = img.convert("L")
     pixels = img.load()
     result = ""
     for i in range(height):
-        for j in range(150):
+        for j in range(cols):
             gray = pixels[j, i]
             index = int(gray / 23)
             result += ascii_chars[index]
